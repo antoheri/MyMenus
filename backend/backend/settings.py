56aa46b0c160ccd586f792e9f680bb94d5ce429a
totti <tottilafrite@protonmail.com>
2025-03-29 09:56:44 +0100

    Add static root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,6 +139,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Directory where static files will be collected during deployment
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
